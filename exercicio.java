8a33de7f5efcd1bb8effff0454d1cbe66f3e5c27
CLaudio Henrique <claudio885henrique@gmail.com>
2023-03-10 01:45:21 -0300

    add exs

---
+++ exercicio.java
@@ -0,0 +1,8 @@
+public class exercicio {
+    public static void main (String[] args) {
+        String nome = "Claudio";
+        int idade = 40;
+        boolean casado = false;
+        System.out.println(nome);
+    }
+}
---
+++ exercicio02.java
@@ -0,0 +1,6 @@
+public class exercicio02 {
+    public static void main (String[] args) {
+        String [][] duasdim = {{"Claudio", "M", "DF"}, {"Lidiane", "F", "GO"}};
+        System.out.println(duasdim[0][0] + " e " + duasdim[1][0]);
+    }
+}
